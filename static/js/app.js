7aad503361f525eb3e85e9f4942bc641686bbdc1
samuelje <jeannanicole1020@gmail.com>
2021-06-29 00:05:44 -0600

    Updated app code. Added style.css file for clean format

--- static/js/app.js
+++ static/js/app.js
@@ -1,91 +1,91 @@
-// Select user input field
+// User input field
 var idSelect = d3.select("#selDataset");
 
-// Select demographic 
+// Demographics table for user input
 var demographicsTable = d3.select("#sample-metadata");
 
-// Select the bar chart div
-var barChart = d3.select("bar");
+// Bar chart div
+var barChart = d3.select("#bar");
 
-// Select the bubble chart div
+// Bubble chart div
 var bubbleChart = d3.select("bubble");
 
-// Select the gauge chart div
-// var gaugeChart = d3.select("gauge");
-
-// Create a function to populate dropdown menu with IDs
+// Function to populate dropdown user input field for sample data
 function init() {
-    // reset prior data
+
+    // Reset data
     resetData();
 
-    // Read samples from JSON data file
+    // Read JSON file
     d3.json("data/samples.json").then((data => {
 
-        // Populate dropdown menu with IDs
-        
-        // Loop over each name in array for dropdown
+        // Populate dropdown with all IDs from sample data
+
+        //  Use for loop to add IDs to dropdown menu
         data.names.forEach((name => {
             var option = idSelect.append("option");
             option.text(name);
         }));
 
-        // First ID from list for initial chart data
-        var initID = idSelect.property("value")
+        // Use first ID value as default for initial dropdown selection
+        var initId = idSelect.property("value")
 
-        // Plot charts with inital ID
-        plotCharts(initID);
+        // Plot charts with ID from dropdown
+        plotCharts(initId);
+
     }));
-};
 
-// Reset data
+}
+
+// Function to reset for new user input
 function resetData() {
 
-    // Clear data
     demographicsTable.html("");
-    barChart.hmtl("");
+    barChart.html("");
     bubbleChart.html("");
-    // gaugeChart.html("");
+
 };
 
-// Create function to read JSON data for plot charts
+// Read JSON for plots and create plots
 function plotCharts(id) {
-    // Read samples from JSON data file
+
     d3.json("data/samples.json").then((data => {
-    
-        // Populate demographics table
+
+        // Generate demographics table
+
+        // Filter for user input of participant ID
         var individualMetadata = data.metadata.filter(participant => participant.id == id)[0];
 
-        // Obtain wash frequency for gauge chart
-        var wfreq = individualMetadata.wfreq;
-
-        // Iterate through each key and value in metadata
+        // Iterate through each key and value in the metadata
         Object.entries(individualMetadata).forEach(([key, value]) => {
-            var newlist = demographicsTable.append("u1");
-            newlist.attr("class", "list-group list-group-flush");
 
-            // Append a li item to the unordered list tag
-            var listItem = newlist.append("li");
+            var newList = demographicsTable.append("ul");
+            newList.attr("class", "list-group list-group-flush");
+
+            var listItem = newList.append("li");
+
             listItem.attr("class", "list-group-item p-1 demo-text bg-transparent");
 
-            // Add key value pair from metadata to demographics list
-            listItem.text($`{key}: ${value}`);
+            listItem.text(`${key}: ${value}`);
+
         });
 
         // Retrieve data for plotting charts
 
-        // Filter samples for ID chosen
+        // Filter for user input of ID
         var individualSample = data.samples.filter(sample => sample.id == id)[0];
 
-        // Empty arrays for sample data
-        var otuIDs = [];
+        // Empty arrays to store data
+        var otuIds = [];
         var otuLabels = [];
         var sampleValues = [];
 
-        // Iterate through each key and value for plotting
+        // Iterate through each key and value in the sample to retrieve data for plotting
         Object.entries(individualSample).forEach(([key, value]) => {
+
             switch (key) {
                 case "otu_ids":
-                    otuIDs.push(value);
+                    otuIds.push(value);
                     break;
                 case "sample_values":
                     sampleValues.push(value);
@@ -93,28 +93,32 @@ function plotCharts(id) {
                 case "otu_labels":
                     otuLabels.push(value);
                     break;
+                    // case
                 default:
                     break;
             }
+
         });
 
-        // Slice and reverse arrays to get top 10 values
-        var topOtuIDs = otuIDs[0].slice(0, 10).reverse();
+        // slice and reverse the arrays to get the top 10 values, labels and IDs
+        var topOtuIds = otuIds[0].slice(0, 10).reverse();
         var topOtuLabels = otuLabels[0].slice(0, 10).reverse();
         var topSampleValues = sampleValues[0].slice(0, 10).reverse();
 
-        // Map function to store IDs with "OTU" for labeling y-axis
-        var topOtuIDsFormatted = topOtuIDs.map(otuID => "OTU " + otuID);
+        // use the map function to store the IDs with "OTU" for labelling y-axis
+        var topOtuIdsFormatted = topOtuIds.map(otuID => "OTU " + otuID);
 
         // Plot bar chart
+
+        // Create trace
         var traceBar = {
             x: topSampleValues,
-            y: topOtuIDsFormatted,
+            y: topOtuIdsFormatted,
             text: topOtuLabels,
             type: 'bar',
             orientation: 'h',
             marker: {
-                color: 'rgb(29, 145, 192)'
+                color: 'rgb(29,145,192)'
             }
         };
 
@@ -149,27 +153,30 @@ function plotCharts(id) {
             }
         }
 
-        // Add bar chart to "bar" div
-        Plotly.newPlot("bar", dataBar, layoutBar);
 
+        // Plot bar chart to "bar" div
+        Plotly.newPlot("bar", dataBar, layoutBar);
+        
         // Plot bubble chart
-        var traceBubble = {
-            x: otuIDs[0],
+
+        // Create trace
+        var traceBub = {
+            x: otuIds[0],
             y: sampleValues[0],
             text: otuLabels[0],
             mode: 'markers',
             marker: {
                 size: sampleValues[0],
-                color: otuIDs[0],
+                color: otuIds[0],
                 colorscale: 'YlGnBu'
             }
         };
 
-        // Create data array for bubble chart
-        var dataBubble = [traceBubble];
+        // Data array
+        var dataBub = [traceBub];
 
-        // Plot layout for bubble chart
-        var layoutBubble = {
+        // Layout for bubble chart
+        var layoutBub = {
             font: {
                 family: 'Quicksand'
             },
@@ -179,25 +186,34 @@ function plotCharts(id) {
                 }
             },
             xaxis: {
-                title: "<b>OTU ID</b>",
-                color: 'rgb(34, 94, 168)'
+                title: "<b>OTU Id</b>",
+                color: 'rgb(34,94,168)'
             },
             yaxis: {
                 title: "<b>Sample Values</b>",
-                color: 'rgb(34, 94, 168)'
+                color: 'rgb(34,94,168)'
             },
             showlegend: false,
         };
 
-        // Plot bubble chart to div
-        Plotly.newPlot('bubble', dataBubble, layoutBubble);
-    ));    
+        // Plot bubble chart to bubble div
+        Plotly.newPlot('bubble', dataBub, layoutBub);
+
+    }));
+
 };
 
+// Create function with ID parameter for when dropdown selection is changed
 function optionChanged(id) {
+
+    // Reset data
     resetData();
 
+    // Plot charts for user input ID
     plotCharts(id);
+
+
 }
 
+// init function used to recall default data
 init();
